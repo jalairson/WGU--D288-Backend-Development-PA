ecbe80f58205ab0aa455da87a2e12c3d3c6cbc60
Jerry <jlairs5@wgu.edu>
2023-07-06 15:52:22 -0400

    test test test

---
+++ src/main/java/com/jerry/javad288/JavaD288Application.java
@@ -0,0 +1,13 @@
+package com.jerry.javad288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaD288Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaD288Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jerry/javad288/JavaD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.javad288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaD288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

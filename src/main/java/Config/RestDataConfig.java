ce1b440dc543d5b5a32b862447ea2de6c11907c5
Jerry <jlairs5@wgu.edu>
2023-07-10 14:19:53 -0400

    part C: creates new packages and config

--- src/main/java/Config/RestDataConfig.java
+++ src/main/java/Config/RestDataConfig.java
@@ -4,7 +4,7 @@ import edu.wgu.d288_backend.entities.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.
 
 /**
  * <h1>RestDataConfig</h1>
